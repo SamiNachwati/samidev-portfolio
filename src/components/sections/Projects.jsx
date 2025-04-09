0a88c8aae6d44dbe2d1e800bbece9414313748d2
loosecode19 <saminachwati94@gmail.com>
2025-04-09 02:56:29 -0400

    Modified Project Section

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -6,6 +6,7 @@ import { RevealOnScroll } from '../RevealOnScroll';
 import nodechat from '../icons/nodechat.png';
 import smartchip from '../icons/smartchip.png';
 import petsphere from '../icons/petsphere.png';
+import portfolio from '../icons/portfolio.png';
 export const Projects = () => {
     return (
         <section id="projects"
@@ -15,6 +16,34 @@ export const Projects = () => {
                     Featured Projects
                 </h2>
                 <div className='max-w-5xl mx-auto px-4 flex flex-wrap gap-5'>
+                <div className="flex flex-col lg:flex-row p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
+                    <img
+                        className="w-full lg:w-1/2 object-cover rounded-lg border-1 border-transparent bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-border"
+                        src={portfolio}
+                        alt="Portfolio"
+                    />
+                    <div className="flex flex-col justify-between p-4 lg:w-1/2">
+                        <h3 className="text-xl font-bold mb-2">
+                            <strong>My Portfolio</strong>
+                        </h3>
+                        <p className="mb-4">
+                        Interactive Portfolio Website
+                        </p>
+                        <div className="flex flex-wrap gap-2 pt-1 pb-3">
+                            {["React", "Vite", "Tailwind CSS"].map((item, index) => (
+                                <span key={index} className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+                                    {item}
+                                </span>
+                            ))}
+                        </div>
+                        <div className='p-2'>
+                            View Project
+                            <a href="https://github.com/SamiNachwati/samidev-portfolio" target="_blank" className='text-blue-400 hover:text-blue-300 transition-colors my-4'>
+                                <FontAwesomeIcon icon={faUpRightFromSquare} className='ml-3'/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
                 <div className="flex flex-col lg:flex-row p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
                         <img
                             className="w-full lg:w-1/2 object-cover rounded-lg border-1 border-transparent bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-border"
@@ -29,7 +58,7 @@ export const Projects = () => {
                             Pet inventory management application designed for store efficiency
                             </p>
                             <div className="flex flex-wrap gap-2 pt-1 pb-3">
-                                {["React", "Vite", "Tailwind CSS", "Node.js", "SQLlite", "Express.js"].map((item, index) => (
+                                {["React", "Vite", "Tailwind CSS", "Node.js", "SQLite", "Express.js"].map((item, index) => (
                                     <span key={index} className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
                                         {item}
                                     </span>
@@ -86,7 +115,7 @@ export const Projects = () => {
                                 Analytical tool that analyzes the best CPU based on user needs
                             </p>
                             <div className="flex flex-wrap gap-2 pt-1 pb-3">
-                                {["Python", "matplot", "Streamlit"].map((item, index) => (
+                                {["Python", "Matplotlib", "Streamlit"].map((item, index) => (
                                     <span key={index} className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
                                         {item}
                                     </span>
