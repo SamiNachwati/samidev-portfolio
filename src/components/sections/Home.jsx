262f2661c03c83dcabad6abf9361d82aaf7803f2
loosecode19 <saminachwati94@gmail.com>
2026-01-26 11:04:52 -0500

    Updated sections of portfolio

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -9,11 +9,9 @@ export const Home = () => {
                     <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-tight">
                         Hi, I'm Sami Nachwati
                     </h1>
-
                     <p className="text-gray-400 text-lg mb-9 max-w-lg mx-auto">
                         I'm a Software Developer who builds thoughtful, user-focused web applications with an emphasis on clarity, performance, and maintainability.
                     </p>
-
                     <div className='flex justify-center space-x-4'>
                     <a href="#projects"
                     className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rba(59, 130, 246, 0.4)] hover:shadow-blue-500/50 group">
